a7d83b8bb39117c6e44d3290b84fe32b38bafe48
Ross <rosshamey@gmail.com>
2023-04-10 18:34:50 -0400

    setting up table

--- createDiagram.py
+++ createDiagram.py
@@ -5,23 +5,33 @@ import pyautogui
 import keyboard
 import torch
 from functools import partial
+from pprint import pprint
 
-
+height, width = 500, 1000
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='./weights/3-937i-692e-m.pt')
 
 # with mss.mss() as sct:
 #     monitor = {'top': 0, 'left': 0, 'width': 1920, 'height': 1080}
+
+def createTable():
+
+    table = np.zeros((height, width, 3), dtype=np.uint8)
+    table[:, :] = (200, 200, 200)
+    table = cv2.cvtColor(table, cv2.COLOR_BGR2RGB)
+
+    return table
+
+def showObjects(objects, background=createTable())
+    
+    
 
 capture = cv2.VideoCapture('./imgs/vid1.mp4')
 
 if (capture.isOpened() == False):
     print('error')
 
-
-
 while (capture.isOpened()):
     ret, frame = capture.read()
-    cv2.imshow('frame', frame)
     keypressed = cv2.waitKey(30)
     if keypressed == ord('q'):
         break
@@ -31,12 +41,22 @@ while (capture.isOpened()):
 
     results = detections.pandas().xyxy[0].to_dict()
 
-    for result in results:
-        x1 = int(result['xmin'])
-        x2 = int(result['xmax'])
-        y1 = int(result['ymin'])
-        y2 = int(result['ymax'])
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+    for i in range(len(results['xmin'])):
+        x1 = int(results['xmin'][i])
+        x2 = int(results['xmax'][i])
+        y1 = int(results['ymin'][i])
+        y2 = int(results['ymax'][i])
+        
+        halfX = (x2 - x1) / 2
+        halfY = (y2 - y1) / 2
+        middleX = x1 + halfX
+        middleY = y1 + halfY
+
+        center = (int(middleX), int(middleY))
+
+        cv2.circle(frame, center, 12, (0, 255, 0), -1)
+        # cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+        cv2.imshow('diagram', frame)
 
     # if len(rl) > 0:
     #     if rl[0][4] > .05:
